facd405ac1747c725cd45bbb4253a9cf9a6a4f22
FeiXie-Liam <xiefei0887@163.com>
2018-08-02 20:48:18 +0800

    [xiefei] fix : fix flyway error at product_table and application.yml

--- src/main/resources/db/migration/V201808022024__init_product_table.sql
+++ src/main/resources/db/migration/V201808022024__init_product_table.sql
@@ -1,7 +1,7 @@
 create table product(
 	id bigint(20) PRIMARY KEY AUTO_INCREMENT,
 	name varchar(50) not null,
-	image varchar(100) not null,
+	image_url varchar(100) not null,
 	unit varchar(50) not null,
 	price double not null
 )charset utf8;
