e010804a2ed0843269be57e6c95e1de18cc09114
FeiXie-Liam <xiefei0887@163.com>
2018-08-02 20:50:16 +0800

    [xiefei] feature : add getAll method for product

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Product;
+import com.example.demo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    ProductService productService;
+
+    @GetMapping
+    public ResponseEntity getAll(){
+        return ResponseEntity.ok(productService.getAll());
+    }
+}
---
+++ src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,47 @@
+package com.example.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "image_url")
+    private String imageUrl;
+    private String unit;
+    private Double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
